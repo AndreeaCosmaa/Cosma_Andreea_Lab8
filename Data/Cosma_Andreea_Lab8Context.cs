5dc57b95cba97d1158b7c1b7dc66dde0121d1774
Cosma Andreea <cosmaandreea_200020@yahoo.com>
2021-11-26 20:34:03 +0200

    Add project files.

---
+++ Data/Cosma_Andreea_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cosma_Andreea_Lab8.Models;
+
+namespace Cosma_Andreea_Lab8.Data
+{
+    public class Cosma_Andreea_Lab8Context : DbContext
+    {
+        public Cosma_Andreea_Lab8Context (DbContextOptions<Cosma_Andreea_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cosma_Andreea_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Cosma_Andreea_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Cosma_Andreea_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Migrations/20211125143923_BookCategory.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cosma_Andreea_Lab8.Migrations
+{
+    public partial class BookCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookCategory",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BookID1 = table.Column<int>(type: "int", nullable: true),
+                    CategoryID = table.Column<int>(type: "int", nullable: false)
+                },
+
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookCategory", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_BookCategory_Book_BookID1",
+                        column: x => x.BookID1,
+                        principalTable: "Book",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_BookCategory_Category_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Category",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookCategory_BookID1",
+                table: "BookCategory",
+                column: "BookID1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookCategory_CategoryID",
+                table: "BookCategory",
+                column: "CategoryID");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Publisher_PublisherID",
+                table: "Book");
+
+            migrationBuilder.DropTable(
+                name: "BookCategory");
+
+            migrationBuilder.DropTable(
+                name: "Publisher");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Book_PublisherID",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "PublisherID",
+                table: "Book");
+        }
+    }
+}
